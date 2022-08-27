1c37591c4f29644756ad7ab504f73f6a9e5b1c4e
IgorEverton <rm95214@fiap.com.br>
2022-08-27 16:10:07 -0300

    Adicionando o componente semaforo

---
+++ src/componentes/carteirinha/Carteirinha.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Carteirinha(){
+    return(
+        <div class="cat1">
+            <img/>
+            <h2>Igor Everton Sobral Alves</h2>
+            <h2>RM: 95214</h2>
+            <h3>1TDSR</h3>
+        </div>
+    )
+}
--- src/componentes/semaforo/Semaforo.jsx
+++ src/componentes/semaforo/Semaforo.jsx
@@ -1,25 +1,77 @@
 import React from "react"
-import { useState } from "react"
+
+
 import './semaforo.css'
 
 export default function Semaforo(){
+
+
+    var i=0
+
+    var cores = {
+
+        'verde':()=>{  
+            const g = window.document.getElementById('verde')
+            g.style.backgroundColor='green' 
+            g.style.color='green' 
+        },
+        'amarelo':()=>{
+            const y =window.document.getElementById('amarelo')
+            y.style.backgroundColor='yellow'  
+            y.style.color='yellow'  
+        },
+        'vermelho':()=>{
+            const r =window.document.getElementById('vermelho')
+            r.style.backgroundColor='red'
+            r.style.color= 'red'
+        },
+        'apagado':()=>{
+            const g = window.document.getElementById('verde')
+            g.style.backgroundColor='rgb(1, 59, 1)'
+            g.style.color='rgb(1, 59, 1)' 
+            const y =window.document.getElementById('amarelo')
+            y.style.backgroundColor='rgb(48, 48, 1)'
+            y.style.color='rgb(48, 48, 1)'
+            const r =window.document.getElementById('vermelho')
+            r.style.backgroundColor='rgb(76, 2, 2)'
+            r.style.color='rgb(76, 2, 2)' 
+        }
+    }
+
+    var mudaCor = ()=>{
+        cores['apagado']()
+        if(i===1){
+            cores['verde']()        
+        }
+
+        else if(i===2){
+            cores['amarelo']()   
+        }
+
+        else if(i===3){
+            cores['vermelho']()
+        }
+
+        else{
+            i=0  
+        }
+        
+        i++
+    }
     
-    
-    const [mudarSema1, setMudarSema1] = useState()
-    const [mudarSema2, setMudarSema2] = useState()
-    const [mudarSema3, setMudarSema3] = useState()
-    
+
     
 
     return(
-        <div>
-        <h2>SEMAFORO: <p class="sema1">{mudarSema1}</p> </h2>
-        <button onClick={()=> setMudarSema1("VERDE")}>MUDAR SEMAFORO</button>
-        <h2>SEMAFORO: <p class="sema2">{mudarSema2}</p></h2>
-        <button onClick={()=> setMudarSema2("AMARELO")}>MUDAR SEMAFORO</button>
-        <h2>SEMAFORO: <p class="sema3">{mudarSema3}</p></h2>
-        <button onClick={()=> setMudarSema3("VERMELHOR")}>MUDAR SEMAFORO</button>
-       
+        <div class="semaforo">
+            <div class="container">
+            <div id="verde"><h2>SIGA</h2></div>
+            <div id="amarelo"><h2>ATENÇÃO</h2></div>
+            <div id="vermelho"><h2>PARE</h2></div>
+            </div>
+            <div id="botao">
+                <button onClick={mudaCor}>mudar cor</button>
+            </div>
     </div>
     )
 }
